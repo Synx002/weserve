e514d8c628550272660228d784535c9a1a242cbf
Mohammad Syam <mosyam0202@gmail.com>
2024-10-15 12:46:27 +0700

    final

--- script.js
+++ script.js
@@ -1,29 +1,30 @@
-const slider = document.getElementById('slider');
-  let isDown = false;
-  let startX;
-  let scrollLeft;
-
-  slider.addEventListener('mousedown', (e) => {
-    isDown = true;
-    slider.classList.add('cursor-grabbing');
-    startX = e.pageX - slider.offsetLeft;
-    scrollLeft = slider.scrollLeft;
+function toggleAccordion(faqId) {
+  const answer = document.getElementById(`faq-answer-${faqId}`);
+  const arrowDown = document.getElementById(`arrow-down-${faqId}`);
+  const arrowUp = document.getElementById(`arrow-up-${faqId}`);
+  const isHidden = answer.classList.contains('hidden');
+  
+  // Close all other answers
+  document.querySelectorAll('[id^="faq-answer"]').forEach(el => {
+    el.classList.add('hidden');
   });
 
-  slider.addEventListener('mouseleave', () => {
-    isDown = false;
-    slider.classList.remove('cursor-grabbing');
+  // Hide all other arrows (reset)
+  document.querySelectorAll('[id^="arrow-down"]').forEach(el => {
+    el.classList.remove('hidden');
+  });
+  document.querySelectorAll('[id^="arrow-up"]').forEach(el => {
+    el.classList.add('hidden');
   });
 
-  slider.addEventListener('mouseup', () => {
-    isDown = false;
-    slider.classList.remove('cursor-grabbing');
-  });
-
-  slider.addEventListener('mousemove', (e) => {
-    if (!isDown) return;
-    e.preventDefault();
-    const x = e.pageX - slider.offsetLeft;
-    const walk = (x - startX) * 2; // Scrolling speed
-    slider.scrollLeft = scrollLeft - walk;
-  });
+  // Toggle the selected answer
+  if (isHidden) {
+    answer.classList.remove('hidden');
+    arrowDown.classList.add('hidden');  // Hide down arrow
+    arrowUp.classList.remove('hidden'); // Show up arrow
+  } else {
+    answer.classList.add('hidden');
+    arrowDown.classList.remove('hidden'); // Show down arrow
+    arrowUp.classList.add('hidden');      // Hide up arrow
+  }
+}
